4857b02586dcef3f711f2196ff595733237c67ab
SkyezerFox <actuallyori@gmail.com>
2021-02-26 20:37:10 +0000

    set up basic gradle-powered papermc plugin

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import kr.entree.spigradle.kotlin.paper
+import kr.entree.spigradle.kotlin.papermc
+
+// declare plugins
+plugins {
+    kotlin("jvm") version "1.4.31"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    // useful plugin for quickly making plugins
+    id("kr.entree.spigradle") version "2.2.3"
+}
+
+// define repos to fetch dependencies from
+repositories {
+    mavenCentral()
+    // paper repository
+    papermc()
+
+    // repositories for command api
+    maven { url = uri("https://raw.githubusercontent.com/JorelAli/CommandAPI/mvn-repo/") }
+    maven { url = uri("https://repo.codemc.org/repository/maven-public/") }
+}
+
+// module information
+version = "1.0.0"
+group = "me.yiffed.veinminer"
+
+// define the actual dependencies
+dependencies {
+    // compileOnly - don't include in the final jar
+    // implementation - include in the final jar
+
+    // want to include the kotlin stdlib - doesn't work without!
+    implementation(kotlin("stdlib"))
+
+    // papermc
+    compileOnly(paper("1.16.5-R0.1-SNAPSHOT"))
+
+    // plugin specific deps - command api
+    compileOnly("dev.jorel", "commandapi-core", "5.8")
+    implementation("dev.jorel", "commandapi-shade", "5.8")
+}
+
+tasks {
+    // shades plugin dependencies into the plugin jar so we can use them
+    // at runtime.
+    shadowJar {
+        archiveClassifier.set("")
+    }
+
+    build {
+        // ensure we shade in dependencies at runtime
+        dependsOn("shadowJar")
+    }
+}
---
+++ src/main/kotlin/me/yiffed/veinminer/VeinMiner.kt
@@ -0,0 +1,31 @@
+package me.yiffed.veinminer
+
+import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPICommand
+import dev.jorel.commandapi.executors.PlayerCommandExecutor
+import kr.entree.spigradle.annotations.PluginMain
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * Root class for the vein miner plugin.
+ */
+@PluginMain
+class VeinMiner : JavaPlugin() {
+    override fun onLoad() {
+        // turn on verbose mode because it's nice
+        CommandAPI.onLoad(true)
+    }
+
+    override fun onEnable() {
+        // always call this!
+        CommandAPI.onEnable(this)
+        // construct a boop command - very useful api, includes tab completion.
+        CommandAPICommand("boop")
+                .executesPlayer(PlayerCommandExecutor { sender, args ->
+                    sender.sendMessage("boop!")
+                }).register()
+        // please try and use this as often as you can - isn't necessary, but helps other people
+        // read your code :3
+        this.logger.info("enabled!")
+    }
+}
